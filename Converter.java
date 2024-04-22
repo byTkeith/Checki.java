4bf2e121bc872fb86af42bf8b2eb424301def063
Your Name <you@example.com>
2024-04-22 23:32:55 +0200

    algorthm

--- Converter.java
+++ Converter.java
@@ -9,6 +9,11 @@ public class Converter {
             String[] splitTime=input.split(":");//splits the time strings into hours and minutes
             int hour= Integer.parseInt(splitTime[0]);//the hour part converted to an int
             int minute= Integer.parseInt(splitTime[1]);//converts the minutes of the time into in
+            /*
+             * our clock uses 5 minute intervals
+             * that is any time below 5 is 'O'clock'
+             * anytime above a 5 mutiple uses the next multiple of five or the previous depending on how far it is
+             */
         }
     }
     
