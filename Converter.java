64931a269bee9faebb397a9c6f38a90a97feb17f
Your Name <you@example.com>
2024-04-23 01:07:59 +0200

    added after hour addition

--- Converter.java
+++ Converter.java
@@ -49,6 +49,14 @@ public class Converter {
             else if(minuteCount>0 && minuteCount<7 && minute%5!=0){
                 System.out.println("about "+minuteString+" past "+hour);
             }
+            else if(minuteCount>0 && minuteCount<7 && minute%5==0){
+                System.out.println(minuteString+" past "+hour);
+            }
+            else if(minuteCount>6 && minute%5!=0){
+                System.out.println("about "+minuteString+" to "+(hour+1));
+            }
+            else if(minuteCount>6 && mi)
+
 
 
 
