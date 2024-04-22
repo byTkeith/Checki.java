f1aba94f8d41c25ca76a2b08b2aa0c9297884b44
Your Name <you@example.com>
2024-04-22 23:24:44 +0200

    added while loop for quit

--- Converter.java
+++ Converter.java
@@ -1,5 +1,14 @@
 import java.util.*;
 import java.io.*;
 public class Converter {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter a time or 'quit':");
+        String input= scanner.nextLine();
+        while (!input.equalsIgnoreCase("Quit")) {
+            String[] splitTime=input.split(":");
+            
+        }
+    }
     
 }
