efab93fbc881b8bc23c29225816b55e4da2c2b2c
Your Name <you@example.com>
2024-04-22 23:27:41 +0200

    converted the hour to string

--- Converter.java
+++ Converter.java
@@ -7,8 +7,8 @@ public class Converter {
         String input= scanner.nextLine();
         while (!input.equalsIgnoreCase("Quit")) {
             String[] splitTime=input.split(":");//splits the time strings into hours and minutes
-            int hour= Integer.parseInt(splitTime[1]);//the hour part converted to an int
-            
+            int hour= Integer.parseInt(splitTime[0]);//the hour part converted to an int
+            int minute= Integer.parseInt(splitTime[1]);//converts the minutes of the time into in
         }
     }
     
