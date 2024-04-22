2faaf95ab0589bdf0bce0d02abcf964b38ea89f3
Your Name <you@example.com>
2024-04-22 23:56:58 +0200

    printed the hour string

--- Converter.java
+++ Converter.java
@@ -7,24 +7,29 @@ public class Converter {
         Scanner scanner= new Scanner(System.in);
         System.out.println("Enter a time or 'quit':");
         String input= scanner.nextLine();
-        while (!input.equalsIgnoreCase("Quit")) {
+        int hour=0;
+        int minute=0;
+        if(!input.equalsIgnoreCase("Quit")) {
             String[] splitTime=input.split(":");//splits the time strings into hours and minutes
-            int hour= Integer.parseInt(splitTime[0]);//the hour part converted to an int
-            int minute= Integer.parseInt(splitTime[1]);//converts the minutes of the time into in
+            hour= Integer.parseInt(splitTime[0]);//the hour part converted to an int
+            minute= Integer.parseInt(splitTime[1]);//converts the minutes of the time into in
             /*
              * our clock uses 5 minute intervals
              * that is any time below 5 is 'O'clock'
              * anytime above a 5 mutiple uses the next multiple of five or the previous depending on how far it is
              */
-            int i;
-            String hourString="";//initiates the hour string to an empty string
-            for(i=1;i<hour;i++){
-                if(i==hour){
-                    hourString=hourArray[i];//if we have looped to the hour value, then that is the string hour value we are looking for
-                }
-            }
-
         }
+        int i;
+        String hourString="";//initiates the hour string to an empty string
+        for(i=0;i<hourArray.length;i++){
+            if(i==hour){
+                hourString=hourArray[i-1];//if we have looped to the hour value, then that is the string hour value we are looking for
+            }
+           
+        }
+        System.out.println("the hour is: "+hourString);
+
+        
     }
     
 }
